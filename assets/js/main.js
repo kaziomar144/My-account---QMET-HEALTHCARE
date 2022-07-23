a2b7f9a76315b792f23c683811209f0a842561fe
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-07-23 13:48:32 +0600

    Overflow Scroling issue solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 // bootstrap tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
