9f1653125fb09b0418e1c5d38d08d054326641f0
Kazi Omar Faruk <kaziomar144@gmail.com>
2022-07-23 22:59:08 +0600

    Change Logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,19 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+/** Mobile Menu */
 
-
+// $(document).ready(function () {
+//   let _mobileMenu = $('.mobileMenu');
+//   let _this = $(this);
+//   $('#mobileNav').click(function (e) { 
+//     e.preventDefault();
+    
+//     let _nav = $('.mobileHandheldDepertment').html();
+//     _mobileMenu.addClass(className);
+//     _mobileMenu.html(_nav);
+//     // _mobileMenu.prepend("");
+   
+//   });
+ 
+// });
